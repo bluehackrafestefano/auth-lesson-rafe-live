0d843c248c96a406a2add3e6f143f5130aade115
rafe <rafe@clarusway.com>
2022-02-03 20:46:44 +0100

    added many things

--- authenticate/urls.py
+++ authenticate/urls.py
@@ -1,4 +1,5 @@
 
+from user_example.views import special
 from django.contrib import admin
 from django.urls import path, include
 
@@ -6,4 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('user_example.urls')),
+    path('special/', special, name='special'),
 ]
